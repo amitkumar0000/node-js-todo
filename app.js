02bbbd9518903c9a3256a88949ba0716df08975f
amit.kumar <amit.ap1924@gmail.com>
2018-09-01 21:24:46 +0530

    added dynamic port

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.set('view engine','ejs')
 app.use(express.static('./public'))
 
 //listen to port
-app.listen(3000)
+
+app.listen(process.env.PORT || 3000)
 
 //fire up todoController
 todoController(app)
 
-console.log('you are listening to port 3000')
+console.log('you are listening to port 3000 '+process.env.PORT)
